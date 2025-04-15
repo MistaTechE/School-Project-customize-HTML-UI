95b2eb7414af9bd1634314709aeed2b9fe366a5e
Evan <emeser3@wgu.edu>
2025-04-14 16:21:57 -1000

    added buyProduct function lines 177-189

--- src/main/java/com/example/demo/controllers/AddProductController.java
+++ src/main/java/com/example/demo/controllers/AddProductController.java
@@ -173,4 +173,18 @@ public class AddProductController {
         theModel.addAttribute("availparts",availParts);
         return "productForm";
     }
+
+    @GetMapping("/buyProduct")
+    public String buyProduct(@RequestParam("productID") int theId, Model theModel) {
+        ProductService productService = context.getBean(ProductServiceImpl.class);
+        Product product2=productService.findById(theId);
+        int inv = product.getInv();
+
+        if (inv == 0) {
+            return "failure";
+        }
+        else {
+            return "success";
+        }
+    }
 }
